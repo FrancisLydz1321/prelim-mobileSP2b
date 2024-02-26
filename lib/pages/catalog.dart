48815b373ad5e1f5281b5639fbf2ea89e732a501
Francis Lydzustre <francislydzustre1321@gmail.com>
2024-02-26 10:29:46 +0800

    asdasdasdqweqweqeq

--- lib/pages/catalog.dart
+++ lib/pages/catalog.dart
@@ -8,23 +8,23 @@ class catalog extends StatefulWidget {
 }
 
 class _catalogState extends State<catalog> {
-  final List<Map<String, dynamic>> gridMap = [
-    {"title": "Dawg", "price": "\$80", "images": '/assets/images/dog_8.jpg'},
+  final List<Map<String, dynamic>> dogList = [
+    {"name": "Dawg", "price": "\$80", "images": '/assets/images/dog_8.jpg'},
     {
-      "title": "My_Dawg",
+      "name": "My_Dawg",
       "price": "\$70",
-      "images": '/assets/images/dog_7.jfif'
+      "images": '/assets/images/dog_9.1.jpg'
     },
-    {
-      "title": "Our_Dawg",
-      "price": "\$60",
-      "images": '/assets/images/dog_6.jfif'
-    },
-    {
-      "title": "Thou_Dawg",
-      "price": "\$50",
-      "images": '/assets/images/dog_5.jfif'
-    },
+    // {
+    //   "name": "Our_Dawg",
+    //   "price": "\$60",
+    //   "images": '/assets/images/dog_12.jpg'
+    // },
+    // {
+    //   "name": "Thou_Dawg",
+    //   "price": "\$50",
+    //   "images": '/assets/images/dog_13.jfif'
+    // },
   ];
 
   @override
@@ -42,42 +42,74 @@ class _catalogState extends State<catalog> {
         const SizedBox(height: 25),
         Container(
           width: MediaQuery.of(context).size.width,
-          margin: const EdgeInsets.only(right: 14.0, left: 14.0),
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          margin: const EdgeInsets.only(right: 10.0),
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10.0),
-            color: Colors.grey,
+            color: Colors.white,
           ),
           child: Column(
             children: [
               Stack(
                 children: [
-                  Container(
-                    height: 154,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      image: const DecorationImage(
-                        image: AssetImage('assets/images/dog_9.1.jpg'),
-                        fit: BoxFit.cover,
-                      ),
+                  // Container(
+                  //   height: 150,
+                  //   width: MediaQuery.of(context).size.width,
+                  //   decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.circular(10.0),
+                  //     image: const DecorationImage(
+                  //       image: AssetImage('assets/images/dog_9.1.jpg'),
+                  //       fit: BoxFit.cover,
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(
+                    height: 250,
+                    // width: 250,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: ((context, index) {
+                        return petCard(
+                          name: dogList[index]['name'],
+                          images: dogList[index]['image'],
+                          price: dogList[index]['price'],
+                        );
+                      }),
+                      itemCount: dogList.length,
                     ),
                   ),
-                  const Positioned(
-                    right: 6,
-                    bottom: 6,
-                    child: CircleAvatar(
-                      radius: 10,
-                      backgroundColor: Colors.white,
-                      child: Icon(Icons.shopping_cart,
-                          size: 10, color: Colors.pink),
-                    ),
-                  ),
+                  // const Positioned(
+                  //   right: 6,
+                  //   bottom: 6,
+                  //   child: CircleAvatar(
+                  //     radius: 10,
+                  //     backgroundColor: Colors.white,
+                  //     child: Icon(Icons.shopping_cart,
+                  //         size: 10, color: Colors.pink),
+                  //   ),
+                  // ),
                 ],
               ),
             ],
           ),
         ),
+        const SizedBox(
+          height: 24,
+        ),
+        // SizedBox(
+        //   height: 150,
+        //   child: ListView.builder(
+        //     scrollDirection: Axis.horizontal,
+        //     itemBuilder: ((context, index) {
+        //       return petCard(
+        //         name: dogList[index]['title'],
+        //         price: dogList[index]['price'],
+        //         image: dogList[index]['image'],
+        //       );
+        //     }),
+        //     itemCount: dogList.length,
+        //   ),
+        // ),
       ],
     ));
   }
@@ -87,19 +119,19 @@ class petCard extends StatelessWidget {
   final String? name;
   // final String? type;
   final String? price;
-  final String? image;
+  final String? images;
 
-  const petCard({super.key, this.name, this.price, this.image});
+  const petCard({super.key, this.name, this.price, this.images});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: MediaQuery.of(context).size.width / 2.3,
       margin: const EdgeInsets.only(right: 14.0, left: 14.0),
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        color: Colors.grey,
+        color: Colors.white,
       ),
       child: Column(
         children: [
@@ -111,26 +143,60 @@ class petCard extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
                   image: DecorationImage(
-                    // image: AssetImage('assets/images/dog_9.1.jpg'),
-                    image: AssetImage(image.toString()),
+                    image: AssetImage('assets/images/dog_12.jpg'),
+                    // image: AssetImage(images.toString()),
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Positioned(
-                right: 6,
-                bottom: 6,
-                child: CircleAvatar(
-                  radius: 10,
-                  backgroundColor: Colors.white,
-                  child:
-                      Icon(Icons.shopping_cart, size: 10, color: Colors.pink),
-                ),
-              ),
+              // Positioned(
+              //   right: 6,
+              //   bottom: 6,
+              //   child: CircleAvatar(
+              //     radius: 10,
+              //     backgroundColor: Colors.white,
+              //     child:
+              //         Icon(Icons.shopping_cart, size: 10, color: Colors.pink),
+              //   ),
+              // ),
             ],
           ),
-          Text(
-            name.toString(),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              name.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              price.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Icon(Icons.shopping_cart, size: 10, color: Colors.white),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child:
+                    Icon(Icons.shopping_cart, size: 13, color: Colors.yellow),
+              ),
+            ],
           ),
         ],
       ),
@@ -160,7 +226,7 @@ Widget getSearch() {
           padding: const EdgeInsets.symmetric(vertical: 6.0),
           margin: const EdgeInsets.only(right: 16.0, left: 16.0),
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0), color: Colors.grey),
+              borderRadius: BorderRadius.circular(20.0), color: Colors.white),
           child: TextFormField(
             decoration: const InputDecoration(
               prefixIcon: Icon(Icons.search),
@@ -227,6 +293,3 @@ Widget getSearch() {
 //     });
 //   }
 // }
-
-
-
